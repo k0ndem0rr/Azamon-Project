b9c5054deec6a191cdb7490bc1660e5105447027
macos.roca <marcos@bookline.ai>
2024-10-08 19:53:16 +0200

    AIMA

--- Azamon/javadoc/package-search-index.js
+++ Azamon/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"IA.Azamon"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"IA.Azamon"}];updateSearchResults();
