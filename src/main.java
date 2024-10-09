1b38ef0ed4072429af83170ad76a69d665962349
jose.angel.conde <jose.angel.conde@estudiantat.upc.edu>
2024-10-09 19:47:35 +0200

    Tanteando con Azamon.jar

--- src/main.java
+++ src/main.java
@@ -1,3 +1,17 @@
+import IA.Azamon.*;
+
 public class main {
-    
+
+    public static void main(String[] args) {
+        IA.Azamon.Paquetes paquetes = new IA.Azamon.Paquetes(5, 0);
+        for (Paquete paquete : paquetes) {
+            System.out.println(paquete.toString());
+            
+        }
+
+        IA.Azamon.Transporte transportes = new IA.Azamon.Transporte(paquetes, 0.2, 0);
+        for (Oferta oferta : transportes) {
+            System.out.println(oferta.toString());
+        }
+    }
 }
