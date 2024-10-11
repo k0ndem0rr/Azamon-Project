10e463253e921bbc7e8e444cc8fb2fe713a894fc
jose.angel.conde <jose.angel.conde@estudiantat.upc.edu>
2024-10-11 20:01:33 +0200

    Primera solucion

--- src/MSN/AzamonMain.java
+++ src/MSN/AzamonMain.java
@@ -6,16 +6,18 @@ import aima.*;
 public class AzamonMain {
 
     public static void main(String[] args) {
-        IA.Azamon.Paquetes paquetes = new IA.Azamon.Paquetes(5, 0);
+        IA.Azamon.Paquetes paquetes = new IA.Azamon.Paquetes(10, 234423432);
         for (Paquete paquete : paquetes) {
             System.out.println(paquete.toString());
         }
 
-        IA.Azamon.Transporte transportes = new IA.Azamon.Transporte(paquetes, 0.1, 0);
+        IA.Azamon.Transporte transportes = new IA.Azamon.Transporte(paquetes, 1, 2342342);
         for (Oferta oferta : transportes) {
             System.out.println(oferta.toString());
         }
 
-        System.out.println(paquetes.get(0).toString());
+        AzamonState state = new AzamonState(paquetes, transportes);
+
+        System.out.println(java.util.Arrays.toString(state.getState()));
     }
 }
--- src/MSN/AzamonState.java
+++ src/MSN/AzamonState.java
@@ -17,9 +17,41 @@ public class AzamonState {
         nOfertas = transportes.size();
         state = new int[nPaquetes];
 
-        // TODO
-        // Inicializar el estado asignado a cada paquete la primera oferta que no esté llena y cumpla su tiempo de entrega
+        double[][] sortedPaquetes = new double[nPaquetes][3];
+        for (int i = 0; i < nPaquetes; i++) {
+            sortedPaquetes[i][0] = paquetes.get(i).getPrioridad();
+            sortedPaquetes[i][1] = paquetes.get(i).getPeso();
+            sortedPaquetes[i][2] = i;
+        }
+
+        java.util.Arrays.sort(sortedPaquetes, new java.util.Comparator<double[]>() {
+            public int compare(double[] a, double[] b) {
+                return Double.compare(a[0], b[0]);
+            }
+        });
+
+        System.out.println(java.util.Arrays.deepToString(sortedPaquetes));
+        
+        int i = 0;
+        for (Oferta oferta : transportes) {
+            double pesoLibre = oferta.getPesomax();
+            boolean nextCabe = true;
+            while (nextCabe && i < nPaquetes) {
+                System.out.println("Peso libre en oferta " + transportes.indexOf(oferta) + ": " + pesoLibre);
+                if (sortedPaquetes[i][1] <= pesoLibre) {
+                    state[(int) sortedPaquetes[i][2]] = transportes.indexOf(oferta);
+                    pesoLibre -= sortedPaquetes[i][1];
+                    i++;
+                }
+                else {
+                    nextCabe = false;
+                }
+            }
+            System.out.println("Paquetes por asignar: " + (nPaquetes - (i+1)));
+        }
     }
 
-
+    public int[] getState() {
+        return state;
+    }
 }
