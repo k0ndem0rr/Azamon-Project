77e6842d1d6d11ebb85c1bf8f34f6bb4a007787a
jose.angel.conde <jose.angel.conde@estudiantat.upc.edu>
2024-10-16 18:29:44 +0200

    Compila

--- src/MSN/AzamonHeuristicFunction.java
+++ src/MSN/AzamonHeuristicFunction.java
@@ -1,5 +1,10 @@
 package MSN;
 
-public class AzamonHeuristicFunction {
-    
+import aima.search.framework.HeuristicFunction;
+
+public class AzamonHeuristicFunction implements HeuristicFunction {
+
+    public double getHeuristicValue(Object state) {
+        return 1;
+    }
 }
--- src/MSN/AzamonMain.java
+++ src/MSN/AzamonMain.java
@@ -1,11 +1,15 @@
 package MSN;
 
+import java.util.List;
+import java.util.Properties;
+import java.util.Iterator;
+
 import IA.Azamon.*;
 
 import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
 import aima.search.framework.Problem;
 import aima.search.informed.HillClimbingSearch;
-import aima
 
 public class AzamonMain {
 
@@ -13,21 +17,38 @@ public class AzamonMain {
 
         AzamonState state = new AzamonState(10, 1, 0);
 
-        /*for (Paquete paquete : state.) {
-            System.out.println(paquete.toString());
-        }
-
-        for (Oferta oferta : transportes) {
-            System.out.println(oferta.toString());
-        }*/
-
-
-        System.out.println(java.util.Arrays.toString(state.getState()));
+        HillClimbingMSN(state);
     }
 
     private static void HillClimbingMSN(AzamonState azamon) {
-        Problem problem = new Problem(azamon, new AzamonSuccesorFunction(), new AzamonGoalTest(), new AzamonHeuristicFunction());
-        Search search = new HillClimbingSearch();
+        try{
+            Problem problem = new Problem(azamon, new AzamonSuccesorFunction(), new AzamonGoalTest(), new AzamonHeuristicFunction());
+            Search search = new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem, search);
 
+            System.out.println();
+            printActions(agent.getActions());
+            printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+        
+    }
+    
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
+    
 }
